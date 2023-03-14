32b774b534824bcb578fd8df1e6d4994b24def57
Michael Nau <michaelwnau@gmail.com>
2023-03-14 13:50:30 -0500

    refactor main.py

--- tutorial/main.py
+++ tutorial/main.py
@@ -2,6 +2,14 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
-async def root():
-    return {"message": "Hello World from FastAPI!"}
+def read_root():
+    return {"Hello": "World"}
+
+# To create a route with a path parameter
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
